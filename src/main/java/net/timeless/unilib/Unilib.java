8d79b7814485b7dc97feff413afc76c40dfb12f8
xavpok <jglrxavpok@gmail.com>
2015-06-26 16:10:45 +0200

    Added BlockProvider and ItemProvider

--- src/main/java/net/timeless/unilib/Unilib.java
+++ src/main/java/net/timeless/unilib/Unilib.java
@@ -1,6 +1,96 @@
 package net.timeless.unilib;
 
-public class Unilib {
+import com.google.common.collect.Lists;
+import com.google.common.reflect.ClassPath;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.*;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.timeless.unilib.common.BaseMod;
+import net.timeless.unilib.common.BlockProvider;
+import net.timeless.unilib.common.ItemProvider;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+
+@Mod(modid = "unilib", name = "Unilib", version = "${unilib_version}")
+public class Unilib extends BaseMod {
+
+    @Mod.EventHandler
+    public void onPreInit(FMLPreInitializationEvent evt) {
+        super.preInitMod(evt);
+        logger.info("Loading Unilib "+ Unilib.getVersion());
+        logger.info("Loading content handlers...");
+        try {
+            // Get the FML mod controller in order to change the current container later
+            LoadController controller = ObfuscationReflectionHelper.getPrivateValue(Loader.class, Loader.instance(), 16);
+            ModContainer container = ObfuscationReflectionHelper.getPrivateValue(LoadController.class, controller, 7);
+
+            // List of all the packages in which not to search for providers.
+            // Loading a class from them at this stage might crash the game!
+            List<String> forbiddenPackages = Lists.newArrayList("com.google.", "com.ibm", "akka.", "org.apache.",
+                    "net.minecraft.", "net.minecraftforge.", "com.intellij.", "com.mojang.", "com.oracle.", "com.sun.",
+                    "io.netty.", "ibxm.", "gnu.trove.", "com.typesafe.", "com.jcraft.", "javaw.", "javafx.",
+                    "org.eclipse.", "java.", "org.lwjgl.", "scala.", "paulscode.", "org.xml.", "org.w3c.", "org.omg.",
+                    "org.objectweb.asm.", "org.jcp.xml.", "org.ietf.jgss.", "oracle.", "netscape.", "sun.",
+                    "net.java.games.", "joptsimple.", "jdk.internal.", "javax.", "tv.twitch.", "sunw.");
+            // Get all the classes from the classloader and look for providers
+            ClassPath path = ClassPath.from(ClassLoader.getSystemClassLoader());
+            for(ClassPath.ClassInfo info : path.getTopLevelClasses()) {
+                try {
+                    boolean isAllowed = true;
+                    for(String s : forbiddenPackages) { // Checks the full class name against the forbidden packages' list
+                        if(info.getName().startsWith(s)) {
+                            isAllowed = false;
+                            break;
+                        }
+                    }
+                    if(!isAllowed) {
+                        continue;
+                    }
+                    Class<?> clazz = Class.forName(info.getName(), false, getClass().getClassLoader());
+                    Object instance = clazz.newInstance();
+                    ModMetadata metadata = new ModMetadata();
+                    ModContainer modContainer = new DummyModContainer(metadata); // Mod container used to change the modid of registred items and blocks
+                    Class<?>[] interfaces = clazz.getInterfaces();
+                    for (Class<?> in : interfaces) { // Search for possible providers, such as BlockProvider or ItemProvider
+                        setContainer(modContainer, controller);
+                        if (in.getCanonicalName().equals(BlockProvider.class.getCanonicalName())) {
+                            BlockProvider provider = ((BlockProvider)instance);
+                            logger.info("Found block provider: " + clazz.getName());
+                            Collection<Block> blocks = provider.createBlocks();
+                            metadata.modId = provider.getModID();
+                            for(Block b : blocks) {
+                                GameRegistry.registerBlock(b, b.getUnlocalizedName().replaceFirst("tile\\.", ""));
+                            }
+                        } else if (in.getCanonicalName().equals(ItemProvider.class.getCanonicalName())) {
+                            ItemProvider provider = ((ItemProvider)instance);
+                            logger.info("Found item provider: " + clazz.getName());
+                            Collection<Item> items = provider.createItems();
+                            metadata.modId = provider.getModID();
+                            for(Item i : items) {
+                                GameRegistry.registerItem(i, i.getUnlocalizedName().replaceFirst("item\\.", ""));
+                            }
+                        }
+                        setContainer(container, controller);
+                    }
+                } catch (InstantiationException ex) {
+                    logger.info("Failed to load potential provider "+info.getName()+" because it has no empty constructor");
+                }
+                catch (Exception e) {
+                    // Shhh, nothing but dreams now
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setContainer(ModContainer container, LoadController controller) {
+        ObfuscationReflectionHelper.setPrivateValue(LoadController.class, controller, container, 7);
+    }
 
     public static String getVersion() {
         return "${unilib_version}";
--- src/main/java/net/timeless/unilib/tests/TestMod.java
+++ src/main/java/net/timeless/unilib/tests/TestMod.java
@@ -1,14 +1,23 @@
 package net.timeless.unilib.tests;
 
+import com.google.common.collect.Lists;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.timeless.unilib.Unilib;
 import net.timeless.unilib.common.BaseMod;
+import net.timeless.unilib.common.BlockProvider;
 import net.timeless.unilib.common.CommonProxy;
+import net.timeless.unilib.common.ItemProvider;
+import net.timeless.unilib.common.blocks.BaseBlock;
+
+import java.util.Collection;
 
 @Mod(modid = TestMod.MODID, version = "1.0", name = "Test Unilib")
-public class TestMod extends BaseMod {
+public class TestMod extends BaseMod implements BlockProvider, ItemProvider {
 
     public static final String MODID = "unilib_test";
 
@@ -16,6 +25,8 @@ public class TestMod extends BaseMod {
     public static TestMod instance;
     @SidedProxy(serverSide = "net.timeless.unilib.common.CommonProxy", clientSide = "net.timeless.unilib.client.ClientProxy")
     public static CommonProxy proxy;
+    private Block test0;
+    private Block test1;
 
     @Mod.EventHandler
     public void onPreInit(FMLPreInitializationEvent evt) {
@@ -24,4 +35,18 @@ public class TestMod extends BaseMod {
         logger.info("Loading Unilib test mod, using Unilib "+ Unilib.getVersion());
     }
 
+    @Override
+    public Collection<Block> createBlocks() {
+        return Lists.newArrayList(test0 = new BaseBlock("test0", Material.rock), test1 = new BaseBlock("test1", Material.rock));
+    }
+
+    @Override
+    public Collection<Item> createItems() {
+        return Lists.newArrayList();
+    }
+
+    @Override
+    public String getModID() {
+        return MODID;
+    }
 }
